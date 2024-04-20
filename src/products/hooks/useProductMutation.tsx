a77a3ff0446f518b87ff2bd56edfd9d53298e0da
Stefano <stefanop21@outlook.es>
2024-04-20 16:54:36 -0500

    (update) invalidateQueries

--- src/products/hooks/useProductMutation.tsx
+++ src/products/hooks/useProductMutation.tsx
@@ -1,17 +1,23 @@
 import Swal from 'sweetalert2';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { productActions } from '..';
 
 export const useProductMutation = () => {
+  const queryClient = useQueryClient();
+
   const mutation = useMutation({
     mutationFn: productActions.createProduct,
-    onSuccess: () => {
+    onSuccess: (data) => {
       Swal.fire({
         title: 'Producto creado',
         icon: 'success',
         timer: 1000,
         timerProgressBar: true,
         showConfirmButton: false,
+      });
+
+      queryClient.invalidateQueries({
+        queryKey: ['products', { filterKey: data.category }],
       });
     },
   });
