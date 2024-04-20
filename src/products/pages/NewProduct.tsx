376c7918b9381001c6cd79853b158932fcb6ce52
Stefano <stefanop21@outlook.es>
2024-04-20 10:58:37 -0500

    (update) formulario con React Hook Form

--- src/products/pages/NewProduct.tsx
+++ src/products/pages/NewProduct.tsx
@@ -1,26 +1,116 @@
 import { Button, Image, Input, Textarea } from '@nextui-org/react';
+import { useEffect, useState } from 'react';
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
+
+interface FormInputs {
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+}
 
 export const NewProduct = () => {
+  const { control, handleSubmit, watch } = useForm<FormInputs>({
+    defaultValues: {
+      title: '',
+      price: 0,
+      description: '',
+      category: "Men's clothing",
+      image: '',
+    },
+  });
+
+  const newImage = watch('image');
+
+  const onSubmit: SubmitHandler<FormInputs> = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="w-full flex-col">
       <h1 className="text-2xl font-bold">Nuevo producto</h1>
 
-      <form className="w-full">
+      <form className="w-full" onSubmit={handleSubmit(onSubmit)}>
         <div className="flex justify-around items-center">
           <div className="flex-col w-[500px]">
-            <Input className="mt-2" type="text" label="Titulo del producto" />
-            <Input className="mt-2" type="number" label="Precio del producto" />
-            <Input className="mt-2" type="url" label="Url del producto" />
-            <Textarea className="mt-2" label="Descripcion del producto" />
-            <select className="rounded-md p-3 mt-2 bg-gray-800 w-full">
-              <option value="men's clothing">Men's clothing</option>
-              <option value="women's clothing">Women's clothing</option>
-              <option value="jewelery">Jewelery</option>
-              <option value="electronics">Electronics</option>
-            </select>
-
+            <Controller
+              control={control}
+              name="title"
+              rules={{ required: true }}
+              render={({ field }) => (
+                <Input
+                  value={field.value}
+                  onChange={field.onChange}
+                  className="mt-2"
+                  type="text"
+                  label="Titulo del producto"
+                />
+              )}
+            />
+            <Controller
+              control={control}
+              name="price"
+              rules={{ required: true }}
+              render={({ field }) => (
+                <Input
+                  value={field.value.toString()}
+                  onChange={(event) =>
+                    field.onChange(event.target.valueAsNumber)
+                  }
+                  className="mt-2"
+                  type="number"
+                  label="Precio del producto"
+                />
+              )}
+            />
+            <Controller
+              control={control}
+              name="image"
+              rules={{ required: true }}
+              render={({ field }) => (
+                <Input
+                  value={field.value}
+                  onChange={field.onChange}
+                  className="mt-2"
+                  type="url"
+                  label="Url del producto"
+                />
+              )}
+            />
+            <Controller
+              control={control}
+              name="description"
+              rules={{ required: true }}
+              render={({ field }) => (
+                <Textarea
+                  value={field.value}
+                  onChange={field.onChange}
+                  className="mt-2"
+                  type="text"
+                  label="Descripcion del producto"
+                />
+              )}
+            />
+            <Controller
+              control={control}
+              name="category"
+              rules={{ required: true }}
+              render={({ field }) => (
+                <select
+                  value={field.value}
+                  onChange={field.onChange}
+                  className="rounded-md p-3 mt-2 bg-gray-800 w-full"
+                >
+                  <option value="men's clothing">Men's clothing</option>
+                  <option value="women's clothing">Women's clothing</option>
+                  <option value="jewelery">Jewelery</option>
+                  <option value="electronics">Electronics</option>
+                </select>
+              )}
+            />
             <br />
-            <Button className="mt-2" color="primary">
+            <Button className="mt-2" color="primary" type="submit">
               Crear
             </Button>
           </div>
@@ -32,7 +122,7 @@ export const NewProduct = () => {
               height: '600px',
             }}
           >
-            <Image src="https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg" />
+            <Image src={newImage} />
           </div>
         </div>
       </form>
